2d6613b8b2f64b4d102e91466ef6d7c3ebaac014
J-haug <jeremiahhaugan@gmail.com>
2025-09-01 17:21:41 -0500

    working on item list

--- index.js
+++ index.js
@@ -46,11 +46,12 @@ function moveBackground(event) {
 function toggleContrast() {
   contrastToggle = !contrastToggle;
   if (contrastToggle) {
-    document.body.classList += " dark-theme";
+    document.body.classList.add("dark-theme");
   } else {
     document.body.classList.remove("dark-theme");
   }
 }
+
 
 function contact(event) {
   event.preventDefault();
